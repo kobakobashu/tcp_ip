efb7d35958d2e3a1078edecf565e3d63c9bf31e3
kobakobashu <kobaya4@fuji.waseda.jp>
2023-02-16 15:10:54 +0900

    add step6

--- microps/ip.h
+++ microps/ip.h
@@ -3,6 +3,16 @@
 
 #include "net.h"
 
+#define IP_VERSION_IPV4 4
+
+#define IP_HDR_SIZE_MIN 20
+#define IP_HDR_SIZE_MAX 60
+
+#define IP_ADDR_LEN 4
+#define IP_ADDR_STR_LEN 16
+
+typedef uint32_t ip_addr_t;
+
 int ip_init(void);
 
 #endif
