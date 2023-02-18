f53055111a17bff4b86dcb0d557a92cc0f739877
kobakobashu <kobaya4@fuji.waseda.jp>
2023-02-19 01:33:46 +0900

    add step6 test

--- microps/ip.h
+++ microps/ip.h
@@ -14,5 +14,8 @@
 typedef uint32_t ip_addr_t;
 
 int ip_init(void);
+int ip_addr_pton(const char *p, ip_addr_t *n);
+char *ip_addr_ntop(ip_addr_t n, char *p, size_t size);
+void ip_dump(const uint8_t *data, size_t len);
 
 #endif
--- microps/unittest/test_ip.cpp
+++ microps/unittest/test_ip.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <arpa/inet.h>
 
 extern "C"
 {
@@ -9,4 +10,43 @@ extern "C"
 TEST(IpInitTest, CorrectInit){
     int result = ip_init();
     EXPECT_EQ(0, result);
+}
+
+TEST(IpAddrPtonTest, ValidAddresses) {
+    ip_addr_t addr;
+
+    EXPECT_EQ(ip_addr_pton("0.0.0.0", &addr), 0);
+    EXPECT_EQ(addr, htonl(0x00000000));
+
+    EXPECT_EQ(ip_addr_pton("1.0.0.0", &addr), 0);
+    EXPECT_EQ(addr, htonl(0x01000000));
+
+    EXPECT_EQ(ip_addr_pton("255.255.255.255", &addr), 0);
+    EXPECT_EQ(addr, htonl(0xFFFFFFFF));
+
+    EXPECT_EQ(ip_addr_pton("192.168.1.1", &addr), 0);
+    EXPECT_EQ(addr, htonl(0xC0A80101));
+
+    EXPECT_EQ(ip_addr_pton("10.0.0.1", &addr), 0);
+    EXPECT_EQ(addr, htonl(0x0A000001));
+}
+
+TEST(IpAddrPtonTest, InvalidAddresses) {
+    ip_addr_t addr;
+
+    EXPECT_EQ(ip_addr_pton("192.168.1.256", &addr), -1);
+    EXPECT_EQ(ip_addr_pton("192.168.1.-1", &addr), -1);
+    EXPECT_EQ(ip_addr_pton("192.168.1", &addr), -1);
+    EXPECT_EQ(ip_addr_pton("192.168.1.", &addr), -1);
+    EXPECT_EQ(ip_addr_pton("192.168.1.1.", &addr), -1);
+    EXPECT_EQ(ip_addr_pton("192.168.1.foo", &addr), -1);
+    EXPECT_EQ(ip_addr_pton("", &addr), -1);
+}
+
+TEST(IPAddrNtopTest, ValidInput) {
+    ip_addr_t n = htonl(0xC0000201);
+    int size = 30;
+    char buf[size];
+
+    EXPECT_STREQ(ip_addr_ntop(n, buf, size), "192.0.2.1");
 }
