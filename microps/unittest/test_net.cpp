8d6603793fecb2b599701aa32b804a55baaeea20
kobakobashu <kobaya4@fuji.waseda.jp>
2023-02-21 15:14:30 +0900

    add step7 test

--- microps/unittest/test_net.cpp
+++ microps/unittest/test_net.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
+#include <sys/socket.h>
 
 extern "C"
 {
     #include "../net.h"
+    #include "../platform/linux/platform.h"
 }
 
 TEST(NetDeviceRegisterTest, RegisterDevice) {
@@ -67,4 +69,65 @@ TEST(NetInputHandlerTest, ProtocolAlreadyRegistered) {
     result = net_protocol_register(type, handler);
 
     EXPECT_EQ(result, -1);
+}
+
+TEST(NetDeviceAddIfaceTest, AddIface) {
+    struct net_device *dev;
+    struct net_iface *iface1, *iface2;
+    int result;
+
+    dev = net_device_alloc();
+    dev->ifaces = NULL;
+    strcpy(dev->name, "eth0");
+
+    iface1 = (struct net_iface*)memory_alloc(sizeof(iface1));
+    iface1->family = AF_INET;
+    iface1->next = NULL;
+
+    iface2 = (struct net_iface*)memory_alloc(sizeof(iface2));
+    iface2->family = AF_INET6;
+    iface2->next = NULL;
+
+    result = net_device_add_iface(dev, iface1);
+    EXPECT_EQ(result, 0);
+
+    result = net_device_add_iface(dev, iface1);
+    EXPECT_EQ(result, -1);
+
+    result = net_device_add_iface(dev, iface2);
+    EXPECT_EQ(result, 0);
+}
+
+TEST(NetDeviceGetIfaceTest, IfaceFound) {
+    struct net_device *dev;
+    struct net_iface *iface1, *iface2;
+    struct net_iface *result;
+
+    dev = net_device_alloc();
+    iface1 = (struct net_iface*)memory_alloc(sizeof(iface1));
+    iface1->family = AF_INET;
+    iface1->next = NULL;
+
+    iface2 = (struct net_iface*)memory_alloc(sizeof(iface2));
+    iface2->family = AF_INET6;
+    iface2->next = NULL;
+
+    net_device_add_iface(dev, iface1);
+    net_device_add_iface(dev, iface2);
+
+    result = net_device_get_iface(dev, AF_INET);
+    EXPECT_EQ(result, iface1);
+
+    result = net_device_get_iface(dev, AF_INET6);
+    EXPECT_EQ(result, iface2);
+}
+
+TEST(NetDeviceGetIfaceTest, IfaceNotFound) {
+    struct net_device *dev;
+    int family = AF_INET;
+
+    dev = net_device_alloc();
+    dev->ifaces = NULL;
+
+    EXPECT_EQ(NULL, net_device_get_iface(dev, family));
 }
