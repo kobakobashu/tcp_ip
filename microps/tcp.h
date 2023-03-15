7458fb98d4b827b9a26b634e1c4f5b8e16fffca6
kobakobashu <kobaya4@fuji.waseda.jp>
2023-03-16 01:52:47 +0900

    add step25

--- microps/tcp.h
+++ microps/tcp.h
@@ -6,5 +6,7 @@
 int tcp_open_rfc793(struct ip_endpoint *local, struct ip_endpoint *foreign, int active);
 int tcp_close(int id);
 int tcp_init(void);
+ssize_t tcp_send(int id, uint8_t *data, size_t len);
+ssize_t tcp_receive(int id, uint8_t *buf, size_t size);
 
 #endif
